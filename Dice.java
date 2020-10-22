a6a3de8c724013e572438749011d4f580e7692eb
Abhinai Sai Putta <70501952+abhinaisai2002@users.noreply.github.com>
2020-10-22 19:02:16 +0530

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class Dice {
+    public static void main(String[] args){
+        int count=0;
+        Random rand = new Random();
+        int dice1,dice2;
+        for(int i=1;i<=10000;i++){
+            dice1 = rand.nextInt(6) + 1;
+            dice2 = rand.nextInt(6) + 1;
+            if(dice1 == dice2)
+                count++;
+        }
+        System.out.println("Total no.of doubles : "+count);
+    } 
+}
